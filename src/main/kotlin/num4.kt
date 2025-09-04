256e4ebf6a23acaabd6fb500639f5b3ebbcb5974
WonderSi <kirillkozlov00@gmail.com>
2025-09-04 10:45:45 +0700

    edit num4

--- src/main/kotlin/num4.kt
+++ src/main/kotlin/num4.kt
@@ -1,10 +1,24 @@
 package org.example
 
+fun isPrime(num: Int): Boolean {
+    var d = 2
+    while (d*d <= num && num % d != 0) {
+        d += 1
+    }
+    return d * d > num
+}
+
 fun main() {
     print("Количество простых чисел: ")
     val input = readln().toInt()
-    
-    for (n in 1..input) {
-        println("$n-ое число: ${n+1}")
+
+    var count = 0
+    var number = 2
+    while (count < input) {
+        if (isPrime(number)) {
+            count++
+            println("$count-ое число: $number")
+        }
+        number++
     }
 }
